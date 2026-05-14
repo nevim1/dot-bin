e4745a511d50d0b20beb27f4af638edfaf665dac
nevim <ondrej.lupisek@gmail.com>
2026-05-15 01:47:42 +0900

    added automatic setting up of bins

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# go over all files in this repo
+for i in $(ls -A); do
+	# skip markdowns, git files and this script
+	mi=$( echo $i | sed -r -e s/.*\.md// -e s/^\.git\(i.*$\|$\)// -e s/setup\.sh// )
+	if [[ ! -z $mi ]] then
+		# check if it already exists in home folder
+		if [ -f ~/.local/bin/$mi ]; then
+			echo $mi was found in your local bins.
+			if read -p "Replace it? (y/N) " conf && [[ "$conf" =~ ^[yY]([eE][sS]?)?$ ]]; then
+				mv ~/.local/bin/$mi ~/.local/bin/."$mi".old
+				ln -s "$PWD"/$mi ~/.local/bin/$mi
+				echo the old version was moved to ~/.local/bin/."$mi".old
+			fi
+		else
+			echo whot "$PWD"/$mi
+			ln -s "$PWD"/$mi ~/.local/bin/$mi
+		fi
+	fi
+done
